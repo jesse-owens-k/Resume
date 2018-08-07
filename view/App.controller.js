e791cf809ecf4d43dce1878afa25406b251ea3ee
Jesse Owens K <jesseowens.k@gmail.com>
2018-08-07 10:01:51 +0000

    Update App.controller.js

--- view/App.controller.js
+++ view/App.controller.js
@@ -1,4 +1,4 @@
-sap.ui.controller("sap.ui.demo.myFiori.view.App", {
+sap.ui.controller("sap.ui.demo.Resume.view.App", {
 	
 	/**
 	 * Navigates to another page
@@ -14,7 +14,7 @@ sap.ui.controller("sap.ui.demo.myFiori.view.App", {
 		if (app.getPage(pageId, master) === null) {
 			var page = sap.ui.view({
 				id : pageId,
-				viewName : "sap.ui.demo.myFiori.view." + pageId,
+				viewName : "sap.ui.demo.Resume.view." + pageId,
 				type : "XML"
 			});
 			page.getController().nav = this;
